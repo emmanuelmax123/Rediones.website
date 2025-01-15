8c55bc8fffd51aefcb3ffdd2ef74f15e4da6777a
emmanuelmax123 <sanyaolukolawole202@gamil.com>
2025-01-15 21:57:18 +0100

    working on footer

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
 const navParents = document.querySelectorAll(".js-navlink");
-const navChildren = document.querySelectorAll(". js-navbar");
+const navChildren = document.querySelectorAll(".js-navbar");
 
 navParents.forEach((navParent) => {
   navParent.addEventListener("click", () => {
@@ -14,5 +14,30 @@ navParents.forEach((navParent) => {
         navChid.classList.remove("opacity-0");
       }
     });
+  });
+});
+
+const questionCont = document.querySelectorAll(".questioncont");
+const answerCont = document.querySelectorAll(".answercont");
+const opensymbol = document.querySelectorAll(".opensign");
+const closesymbol = document.querySelectorAll(".closesign");
+questionCont.forEach((question, index) => {
+  question.addEventListener("click", () => {
+    const ishidden = answerCont[index].classList.contains("hidden");
+    answerCont.forEach((answer, i) => {
+      answer.classList.add("hidden");
+      opensymbol[i]?.setAttribute("class", "opensign");
+      closesymbol[i]?.setAttribute("class", "closesign hidden");
+    });
+    if (ishidden) {
+      console.log("clicked");
+      answerCont[index].classList.remove("hidden");
+      closesymbol[index]?.setAttribute("class", "closesign");
+      opensymbol[index]?.setAttribute("class", "opensign hidden");
+    } else {
+      answer.classList.add("hidden");
+      opensymbol[i]?.setAttribute("class", "opensign");
+      closesymbol[i]?.setAttribute("class", "closesign hidden");
+    }
   });
 });
