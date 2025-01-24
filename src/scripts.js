bfee0d4979e0fe05d8df530cbcec73a5725fa101
emmanuelmax123 <sanyaolukolawole202@gamil.com>
2025-01-24 23:30:35 +0100

    made the waitlist response and adjusted the mb of some divs

--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,9 @@ navParents.forEach((navParent, index) => {
   });
 });
 
+// when the js-nav reaches the scroll loaction of the href location
+//change the nav bar
+
 // contains faq data
 const faqdata = [
   {
@@ -187,15 +190,11 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
-function showdialog() {
-  const dialog = document.getElementById("dialog");
-  dialog.show();
-}
-
 function joinwaitlist() {
-  const dialog = document.getElementById("dialog");
-  const maindialog = document.getElementById("maindialog");
-  const subdialog = document.getElementById("subdialog");
+  const modal = document.getElementById("modal");
+  modal.showModal();
+  const mainmodal = document.getElementById("mainmodal");
+  const submodal = document.getElementById("submodal");
   const nameInput = document.getElementById("usrname");
   const emailInput = document.getElementById("usremail");
 
@@ -210,20 +209,44 @@ function joinwaitlist() {
     const emailData = { email: email, name: name };
 
     emailjs.send(serviceID, templateID, emailData).then((res) => {
-      subdialog.classList.remove("hidden");
-      maindialog.classList.add("hidden");
-      subdialog.innerHTML = `
-          <h1 class="text-[32px] mb-8">You're in the waitlist</h1>
-          <p class="w-[393px] mb-9">
-            Thanks for signing up! A confirmation email has been sent to ${email}—be sure to check your inbox (or spam folder).
+      submodal.classList.remove("hidden");
+      mainmodal.classList.add("hidden");
+      submodal.innerHTML = `
+          <div class="w-full flex justify-end cursor-pointer">
+            <svg
+              onclick="document.getElementById('modal').close()"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="18"
+              height="18"
+              fill="currentColor"
+            >
+              <path
+                d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"
+              ></path>
+            </svg>
+          </div>
+          <h1 class="max-sm:text-[20px] text-[32px] mb-4">
+            You're in the waitlist
+          </h1>
+          <p class="w-full mb-9">
+            Thanks for signing up! A confirmation email has been sent to
+            ${email}—be sure to check your inbox (or spam folder).
           </p>
-          <div class="w-[393px]">
+          <div class="max-xxs:w-[254px] w-[300px]">
             <p class="mb-2">Invite Your Friends</p>
             <div class="flex justify-between">
-              <input type="text" class="invitebtn" />
+              <input
+                id="copytext"
+                type="text"
+                class="invitebtn"
+                value="www.rediones.com"
+                readonly
+              />
               <button
+                id="copybtn"
                 type="button"
-                class="bg-white text-black text-[16px] rounded-lg w-[94px] h-[40px]"
+                class="bg-white text-black text-[16px] rounded-lg w-[84px] h-[40px]"
               >
                 Copy link
               </button>
@@ -232,10 +255,18 @@ function joinwaitlist() {
       nameInput.value = "";
       emailInput.value = "";
       setTimeout(() => {
-        dialog.close();
-      }, 5000);
+        modal.close();
+      }, 60000);
     });
   } else {
     console.log("something went wrong");
   }
 }
+
+const copybtn = document.getElementById("copybtn");
+copybtn.addEventListener("click", () => {
+  const copytext = document.getElementById("copytext").value;
+  navigator.clipboard.writeText(copytext);
+  copybtn.style.color = "green";
+  copybtn.innerText = "Copied";
+});
